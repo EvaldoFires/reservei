bc3055d7cf72135070471941cf77a148bbbd11c2
EvaldoFires <125889031+EvaldoFires@users.noreply.github.com>
2025-03-15 09:40:55 -0300

    Adicionado Classes Controller

---
+++ src/main/java/br/com/reservei/api/controller/ReservaController.java
@@ -0,0 +1,78 @@
+package br.com.reservei.api.controller;
+
+import br.com.reservei.api.dto.ReservaDTO;
+import br.com.reservei.api.service.ReservaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/reserva")
+@RequiredArgsConstructor
+public class ReservaController {
+
+    private final ReservaService reservaService;
+
+    @GetMapping("/{idReserva}")
+    @Operation(summary = "Buscar Reserva por ID", description = "Busca uma Reserva pelo seu ID")
+            @ApiResponse(responseCode = "200", description = "Reserva encontrada com sucesso",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ReservaDTO.class)))
+            @ApiResponse(responseCode = "404", description = "Reserva nao encontrada")
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    public ResponseEntity<ReservaDTO> buscarPorId(@PathVariable Long idReserva){
+        return ResponseEntity.ok(reservaService.buscarPorId(idReserva));
+    }
+    
+    @GetMapping
+    @Operation(summary = "Buscar todas as Reservas", description = "Busca uma lista com todas as Reservas")
+    @ApiResponse(responseCode = "200", description = "Reservas encontradas com sucesso",
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = ReservaDTO.class)))
+    @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    public ResponseEntity<List<ReservaDTO>> buscarTodos(){
+        return ResponseEntity.ok(reservaService.buscarTodos());
+    }
+    
+    @PostMapping
+    @Operation(summary = "Salva uma Reserva", description = "Salva uma Reserva")
+    @ApiResponse(responseCode = "201", description = "Reserva salva com sucesso",
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = ReservaDTO.class)))
+    @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    @ApiResponse(responseCode = "400", description = "Requisição invalida")
+    public ResponseEntity<ReservaDTO> salvar(@RequestBody ReservaDTO reservaDTO){
+        reservaDTO = reservaService.salvar(reservaDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(reservaDTO);
+    }
+    
+    @PutMapping("/{idReserva}")
+    @Operation(summary = "Atualiza uma Reserva", description = "Atualiza uma Reserva")
+    @ApiResponse(responseCode = "201", description = "Reserva atualizada com sucesso",
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = ReservaDTO.class)))
+    @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    @ApiResponse(responseCode = "400", description = "Requisição invalida")
+    public ResponseEntity<ReservaDTO> atualizar(@PathVariable Long idReserva, @RequestBody ReservaDTO reservaDTO){
+        reservaDTO = reservaService.atualizar(idReserva, reservaDTO);
+        return ResponseEntity.ok(reservaDTO);
+    }
+    
+    @DeleteMapping("/{idReserva}")
+    @Operation(summary = "Deletar uma Reserva", description = "Deleta uma Reserva")
+    @ApiResponse(responseCode = "204", description = "Reserva deletada com sucesso")
+    @ApiResponse(responseCode = "404", description = "Reserva nao encontrada")
+    @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    public ResponseEntity<Void> deletarPorId(@PathVariable Long idReserva){
+        reservaService.deletarPorId(idReserva);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/main/java/br/com/reservei/api/controller/RestauranteController.java
@@ -0,0 +1,78 @@
+package br.com.reservei.api.controller;
+
+import br.com.reservei.api.dto.RestauranteDTO;
+import br.com.reservei.api.service.RestauranteService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/restaurante")
+@RequiredArgsConstructor
+public class RestauranteController {
+
+    private final RestauranteService restauranteService;
+
+    @GetMapping("/{idRestaurante}")
+    @Operation(summary = "Buscar Restaurante por ID", description = "Busca um Restaurante pelo seu ID")
+            @ApiResponse(responseCode = "200", description = "Restaurante encontrado com sucesso",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = RestauranteDTO.class)))
+            @ApiResponse(responseCode = "404", description = "Restaurante nao encontrado")
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    public ResponseEntity<RestauranteDTO> buscarPorId(@PathVariable Long idRestaurante){
+        return ResponseEntity.ok(restauranteService.buscarPorId(idRestaurante));
+    }
+
+    @GetMapping
+    @Operation(summary = "Buscar todos os Restaurantes", description = "Busca uma lista com todos os Restaurantes")
+    @ApiResponse(responseCode = "200", description = "Restaurantes encontrados com sucesso",
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = RestauranteDTO.class)))
+    @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    public ResponseEntity<List<RestauranteDTO>> buscarTodos(){
+        return ResponseEntity.ok(restauranteService.buscarTodos());
+    }
+
+    @PostMapping
+    @Operation(summary = "Salva um Restaurante", description = "Salva um Restaurante")
+    @ApiResponse(responseCode = "201", description = "Restaurante salvo com sucesso",
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = RestauranteDTO.class)))
+    @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    @ApiResponse(responseCode = "400", description = "Requisição invalida")
+    public ResponseEntity<RestauranteDTO> salvar(@RequestBody RestauranteDTO restauranteDTO){
+        restauranteDTO = restauranteService.salvar(restauranteDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(restauranteDTO);
+    }
+
+    @PutMapping("/{idRestaurante}")
+    @Operation(summary = "Atualiza um Restaurante", description = "Atualiza um Restaurante")
+    @ApiResponse(responseCode = "201", description = "Restaurante atualizado com sucesso",
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = RestauranteDTO.class)))
+    @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    @ApiResponse(responseCode = "400", description = "Requisição invalida")
+    public ResponseEntity<RestauranteDTO> atualizar(@PathVariable Long idRestaurante, @RequestBody RestauranteDTO restauranteDTO){
+        restauranteDTO = restauranteService.atualizar(idRestaurante, restauranteDTO);
+        return ResponseEntity.ok(restauranteDTO);
+    }
+
+    @DeleteMapping("/{idRestaurante}")
+    @Operation(summary = "Deletar um Restaurante", description = "Deleta um Restaurante")
+    @ApiResponse(responseCode = "204", description = "Restaurante deletado com sucesso")
+    @ApiResponse(responseCode = "404", description = "Restaurante nao encontrado")
+    @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    public ResponseEntity<Void> deletarPorId(@PathVariable Long idRestaurante){
+        restauranteService.deletarPorId(idRestaurante);
+        return ResponseEntity.noContent().build();
+    }
+}
